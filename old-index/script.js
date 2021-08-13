560e63c6e4cc25dfe38410d80002e6845ccc8ed8
dibakar <sigdeldkr@gmail.com>
2021-08-13 17:07:28 -0500

    img

--- old-index/script.js
+++
@@ -1,6 +0,0 @@
-var list = document.querySelectorAll(".item a");
-
-for (var i = 0; i < list.length; i++) {
-	var url = list[i].children[0].getAttribute("src");
-  list[i].style.backgroundImage="url('" + url + "')";
-}
